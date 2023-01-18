f4acb392d7b56b10e561580e12672e623dfe66a4
Evgenij (Eugene) Beloded <belodedevgenij@gmail.com>
2023-01-18 07:39:39 +0700

    Remove svelte-preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,8 @@
 import adapter from "@sveltejs/adapter-auto";
-import preprocess from "svelte-preprocess";
 import mm from "micromatch";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
-
   kit: {
     adapter: adapter(),
   },
